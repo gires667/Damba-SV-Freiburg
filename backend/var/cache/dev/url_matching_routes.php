6bbab6957e841af996ab1df2f79636a32e652aa8
gires667 <gires8585@gmail.com>
2026-01-15 16:41:14 +0100

    FEAT:'securing my .env and .env.dev in a gitignore file'

---
+++ backend/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/api/media' => [[['_route' => 'api_media_user', '_controller' => 'App\\Controller\\MediaController::createMedia'], null, ['POST' => 0], null, false, false, null]],
+        '/test' => [[['_route' => 'app_test', '_controller' => 'App\\Controller\\TestController::index'], null, null, null, false, false, null]],
+        '/api/users' => [[['_route' => 'api_create_user', '_controller' => 'App\\Controller\\UserController::createUser'], null, ['POST' => 0], null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
